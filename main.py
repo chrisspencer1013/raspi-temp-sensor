da451921bde0f85c9a03d5ccfa58debdfb6fdb18
Chris Spencer <chris.spencer@freightverify.com>
2022-01-10 22:31:03 -0500

    missing :

--- main.py
+++ main.py
@@ -6,9 +6,11 @@ import colorsys
 import os
 import sys
 import ST7735
+
 try:
     # Transitional fix for breaking change in LTR559
     from ltr559 import LTR559
+
     ltr559 = LTR559()
 except ImportError:
     import ltr559
@@ -21,9 +23,10 @@ from PIL import ImageFont
 from fonts.ttf import RobotoMedium as UserFont
 
 logging.basicConfig(
-    format='%(asctime)s.%(msecs)03d %(levelname)-8s %(message)s',
+    format="%(asctime)s.%(msecs)03d %(levelname)-8s %(message)s",
     level=logging.INFO,
-    datefmt='%Y-%m-%d %H:%M:%S')
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
 
 logging.info("""initializing...""")
 
@@ -31,14 +34,7 @@ logging.info("""initializing...""")
 bme280 = BME280()
 
 # Create ST7735 LCD display class
-st7735 = ST7735.ST7735(
-    port=0,
-    cs=1,
-    dc=9,
-    backlight=12,
-    rotation=270,
-    spi_speed_hz=10000000
-)
+st7735 = ST7735.ST7735(port=0, cs=1, dc=9, backlight=12, rotation=270, spi_speed_hz=10000000)
 
 # Initialize display
 st7735.begin()
@@ -47,23 +43,24 @@ WIDTH = st7735.width
 HEIGHT = st7735.height
 
 # Set up canvas and font
-img = Image.new('RGB', (WIDTH, HEIGHT), color=(0, 0, 0))
+img = Image.new("RGB", (WIDTH, HEIGHT), color=(0, 0, 0))
 draw = ImageDraw.Draw(img)
 path = os.path.dirname(os.path.realpath(__file__))
 font_size = 20
 font = ImageFont.truetype(UserFont, font_size)
 
 
-
 # Get the temperature of the CPU for compensation
 def get_cpu_temperature():
-    process = Popen(['vcgencmd', 'measure_temp'], stdout=PIPE, universal_newlines=True)
+    process = Popen(["vcgencmd", "measure_temp"], stdout=PIPE, universal_newlines=True)
     output, _error = process.communicate()
-    return float(output[output.index('=') + 1:output.rindex("'")])
+    return float(output[output.index("=") + 1 : output.rindex("'")])
+
 
 CPU_TEMPS = [get_cpu_temperature()] * 5
 
-def get_adjusted_room_temp():   
+
+def get_adjusted_room_temp():
     # Tuning factor for compensation. Decrease this number to adjust the
     # temperature down, and increase to adjust up
     factor = 2.25
@@ -71,7 +68,7 @@ def get_adjusted_room_temp():
     cpu_temp = get_cpu_temperature()
 
     # Smooth out with some averaging to decrease jitter
-    nonlocal CPU_TEMPS 
+    nonlocal CPU_TEMPS
     CPU_TEMPS = CPU_TEMPS[1:] + [CPU_TEMPS]
     avg_cpu_temp = sum(CPU_TEMPS) / float(len(CPU_TEMPS))
 
@@ -80,14 +77,14 @@ def get_adjusted_room_temp():
     adjusted_temp = raw_temp - ((avg_cpu_temp - raw_temp) / factor)
 
     # return in F
-    return adjusted_temp * 1.8 + 32 
-
+    return adjusted_temp * 1.8 + 32
 
 
 def update_display(temp, humidity):
     draw.text((0, 0), f"Temp: {temp}°F", font=font, fill=(0, 45, 255))
     draw.text((0, 25), f"Humidity: {humidity}% ", font=font, fill=(0, 45, 255))
     st7735.display(img)
+
 
 def main():
     try:
@@ -100,5 +97,6 @@ def main():
     except KeyboardInterrupt:
         sys.exit(0)
 
-if __name__ == '__main__'
+
+if __name__ == "__main__":
     main()
